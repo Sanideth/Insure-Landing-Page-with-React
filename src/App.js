f06a536a4f28ecc0392cd560cc6247450a66f4b1
Sandro Bochorishvili <Sanideth@gmail.com>
2020-12-12 01:45:26 +0400

    added react core components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import "./css/App.css";
 
-import './css/App.css';
-
-function App() {
-  return (
-    <div className="">Something</div>
-  );
-}
+const App = () => (
+  <>
+    <Header />
+    <Main />
+    <Footer />
+  </>
+);
 
 export default App;
